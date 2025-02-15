e13f4f1aa651332d46dd820a5e624e27d12cd68a
brodricyoung <you22030@byui.edu>
2025-02-15 13:20:10 -0700

    completed lab 6

---
+++ ecen260-stm32/.metadata/.plugins/org.eclipse.core.resources/.history/59/10548be953eb001f1eccc855951be742
@@ -0,0 +1,48 @@
+
+
+
+// each line has offset of 4 bytes (integer type)
+typedef struct {
+	unsigned int MODER;		// offset: 0x00
+	unsigned int OTYPER;	// offset: 0x04
+	unsigned int OSPEEDR;	// offset: 0x08
+	unsigned int PUPDR;		// offset: 0x0C
+	unsigned int IDR;		// offset: 0x10
+	unsigned int ODR;		// offset: 0x14
+} Port;
+
+
+// address of AHB2ENR
+unsigned int* AHB2ENR = (unsigned int*) 0x4002104C;
+
+// base addresses of the GPIO Port SFRs (I/O control registers)
+Port* GPIOA = (Port*) 0x48000000;
+Port* GPIOC = (Port*) 0x48000800;
+
+// Macros
+#define B1PIN 13
+#define LD2PIN 5
+
+int main(void) {
+	// turn of the GPIO clocks for PA (bit 0) and PC (bit 2)
+	*AHB2ENR |= 0b101; // set bits 0 and 2
+
+	// configure B1 (PC13) as input (bits 26 and 27)
+	GPIOC->MODER &= ~(0b11 << (B1PIN * 2)); // reset bits 26 and 27
+
+	// configure LD2 (PA5) as output (bits 11 and 10)
+	GPIOA->MODER &= ~(1 << (LD2PIN * 2)); // reset bit 11
+	GPIOA->MODER |= (2 << (LD2PIN * 2)); // set bit 10
+
+	while(1){
+		// check status of button
+		if((GPIOC->IDR & (1 << B1PIN)) == 0) { // if button pushed
+			// turn on LD2 (PA5)
+			GPIOA->ODR |= (1 << LD2PIN); // set bit 5
+		}
+		else { // if button not pushed
+			// turn off LD2 (PA5)
+			GPIOA->ODR &= ~(1 << LD2PIN); // reset bit 5
+		}
+	}
+}
--- ecen260-stm32/Lab6_alarm_system/Src/main.c
+++ ecen260-stm32/Lab6_alarm_system/Src/main.c
@@ -1,48 +1,139 @@
+/* This program uses inputs from buttons and reed switch sensors
+ * and outputs to status and alarm LEDs to create a home
+ * security system.
+ */
 
+// INSTRUCTIONS FOR 2-PERSON TEAM:
+// Person A needs to type parts 1, 3, and 5.
+// Person B needs to type parts 2, 4, and 6.
 
+// INSTRUCTIONS FOR 3-PERSON TEAM:
+// Person A needs to type parts 1 and 4.
+// Person B needs to type parts 2 and 5.
+// Person C needs to type parts 3 and 6.
 
-// each line has offset of 4 bytes (integer type)
+// GPIO Port struct
 typedef struct {
-	unsigned int MODER;		// offset: 0x00
-	unsigned int OTYPER;	// offset: 0x04
-	unsigned int OSPEEDR;	// offset: 0x08
-	unsigned int PUPDR;		// offset: 0x0C
-	unsigned int IDR;		// offset: 0x10
-	unsigned int ODR;		// offset: 0x14
+    unsigned int MODER;   // offset: 0x00
+    unsigned int OTYPER;  // offset: 0x04
+    unsigned int OSPEEDR; // offset: 0x08
+    unsigned int PUPDR;   // offset: 0x0C
+    unsigned int IDR;     // offset: 0x10
+    unsigned int ODR;     // offset: 0x14
 } Port;
 
+// Address of the Advanced High-performance Bus 2 Enable Register
+unsigned int* AHB2ENR = (unsigned int*) 0x4002104c;
 
-// address of AHB2ENR
-unsigned int* AHB2ENR = (unsigned int*) 0x4002104C;
+// Base addresses of the GPIO Port control registers (SFRs)
+Port* GPIOA = (Port*) 0x48000000; // base address of GPIOA
+Port* GPIOB = (Port*) 0x48000400; // base address of GPIOB
+Port* GPIOC = (Port*) 0x48000800; // base address of GPIOC
 
-// base addresses of the GPIO Port SFRs (I/O control registers)
-Port* GPIOA = (Port*) 0x48000000;
-Port* GPIOC = (Port*) 0x48000800;
+/* Part 1. Typed by Brodric Young. */
 
-// Macros
-#define B1PIN 13
-#define LD2PIN 5
+// input pins
+#define ArmButtonPin 13       // PC13
+#define DisarmButtonPin 0     // PA0
+#define FrontSensorPin 1      // PA1
+#define BackSensorPin 4       // PA4
+#define WindowSensorPin 8     // PA8
+
+// output pins
+#define ArmStatusPin 5        // PA5
+#define FrontAlarmPin 4       // PB4
+#define BackAlarmPin 5        // PB5
+#define WindowAlarmPin 3      // PB3
 
 int main(void) {
-	// turn of the GPIO clocks for PA (bit 0) and PC (bit 2)
-	*AHB2ENR |= 0b101; // set bits 0 and 2
+    // Turn on GPIO clocks (Ports A, B, and C)
+    *AHB2ENR |= 0b111;
 
-	// configure B1 (PC13) as input (bits 26 and 27)
-	GPIOC->MODER &= ~(0b11 << (B1PIN * 2)); // reset bits 26 and 27
+    /* Part 2. Typed by Karl Richards. */
 
-	// configure LD2 (PA5) as output (bits 11 and 10)
-	GPIOA->MODER &= ~(1 << (LD2PIN * 2)); // reset bit 11
-	GPIOA->MODER |= (2 << (LD2PIN * 2)); // set bit 10
+    // Configure button pin as inputs
+    GPIOC->MODER &= ~(0b11 << (ArmButtonPin * 2));  // configure ArmButtonPin as input
+    GPIOA->MODER &= ~(0b11 << (DisarmButtonPin * 2));  // configure DisarmButtonPin as input
 
-	while(1){
-		// check status of button
-		if((GPIOC->IDR & (1 << B1PIN)) == 0) { // if button pushed
-			// turn on LD2 (PA5)
-			GPIOA->ODR |= (1 << LD2PIN); // set bit 5
-		}
-		else { // if button not pushed
-			// turn off LD2 (PA5)
-			GPIOA->ODR &= ~(1 << LD2PIN); // reset bit 5
-		}
-	}
+    // Configure reed switch pins as inputs
+    GPIOA->MODER &= ~(0b11 << (FrontSensorPin * 2));
+    GPIOA->MODER &= ~(0b11 << (BackSensorPin * 2));
+    GPIOA->MODER &= ~(0b11 << (WindowSensorPin * 2));
+
+    /* Part 3. Typed by Brenden Clark. */
+
+    // Enable pull resistors
+    GPIOA->PUPDR |= (0b01 << (DisarmButtonPin * 2));
+    GPIOA->PUPDR |= (0b01 << (FrontSensorPin * 2));
+    GPIOA->PUPDR |= (0b01 << (BackSensorPin * 2));
+    GPIOA->PUPDR |= (0b01 << (WindowSensorPin * 2));
+
+    /* Part 4. Typed by Brodric Young. */
+
+    // Configure LED pins as outputs
+    GPIOA->MODER &= ~(0b10 << (ArmStatusPin * 2));  // configure ArmStatusPin as output
+    GPIOB->MODER &= ~(0b10 << (FrontAlarmPin * 2));  // configure FrontAlarmPin as output
+    GPIOB->MODER &= ~(0b10 << (BackAlarmPin * 2));  // configure BackAlarmPin as output
+    GPIOB->MODER &= ~(0b10 << (WindowAlarmPin * 2));  // configure WindowAlarmPin as output
+
+    GPIOA->MODER |= (0b1 << (ArmStatusPin * 2));  // configure ArmStatusPin as output
+    GPIOB->MODER |= (0b1 << (FrontAlarmPin * 2));  // configure FrontAlarmPin as output
+    GPIOB->MODER |= (0b1 << (BackAlarmPin * 2));  // configure BackAlarmPin as output
+    GPIOB->MODER |= (0b1 << (WindowAlarmPin * 2));  // configure WindowAlarmPin as output
+
+    // Initialize Alarm Status (turn off all LEDs)
+    GPIOA->ODR &= ~(1 << ArmStatusPin);
+    GPIOB->ODR &= ~(1 << FrontAlarmPin);
+    GPIOB->ODR &= ~(1 << BackAlarmPin);
+    GPIOB->ODR &= ~(1 << WindowAlarmPin);
+
+    // armed state (initialize to disarm)
+    int armed = 0;
+
+    // infinite loop
+    while (1) {
+
+        /* Part 5. Typed by Karl Richards. */
+
+        // check if ArmButton is pressed
+        if ((GPIOC->IDR & (1 << 13)) == 0) {
+            // arm system
+            armed = 1;  // set arm state
+            GPIOA->ODR |= (1 << 5);  // turn on ArmStatus LED
+        }
+
+        // check if DisarmButton is pressed
+        if ((GPIOA->IDR & (1 << DisarmButtonPin)) == 0) {
+            // disarm system
+            armed = 0;  // reset arm state
+            GPIOA->ODR &= ~(1 << ArmStatusPin);
+            GPIOB->ODR &= ~(1 << FrontAlarmPin);
+            GPIOB->ODR &= ~(1 << BackAlarmPin);
+            GPIOB->ODR &= ~(1 << WindowAlarmPin);
+        }
+
+        /* Part 6. Typed by Brenden Clark. */
+
+        // if system is armed, check sensors
+        if (armed) {
+
+            // check the FrontSensor
+            if (!(GPIOA->IDR & (1 << FrontSensorPin)) == 0) {
+                // system armed & front door is open:
+                GPIOB->ODR |= (1 << FrontAlarmPin);  // turn on FrontAlarm LED
+            }
+
+            // check the BackSensor
+            if (!(GPIOA->IDR & (1 << BackSensorPin)) == 0) {
+                // system armed & back door is open:
+                GPIOB->ODR |= (1 << BackAlarmPin);  // turn on BackAlarm LED
+            }
+
+            // check the WindowSensor
+            if (!(GPIOA->IDR & (1 << WindowSensorPin)) == 0) {
+                // system armed & window is open:
+                GPIOB->ODR |= (1 << WindowAlarmPin);  // turn on WindowAlarm LED
+            }
+        }
+    }
 }
